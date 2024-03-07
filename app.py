67dd2c1a032fb9d2d8136b67e6c2ba5f1ac9f43e
karthikowo <20pw16@psgtech.ac.in>
2024-03-07 23:02:36 +0530

    register bp

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
 from controllers.base_controller import base_bp
+from db.connection import DatabaseConnection
+from controllers.api.pdf_controller import pdf_bp
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
 app.register_blueprint(base_bp)
+app.register_blueprint(pdf_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
