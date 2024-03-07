0811260be5c6f1edd793368f59361961a9d764dd
karthikowo <20pw16@psgtech.ac.in>
2024-03-07 13:59:02 +0530

    Migrate to Flask :(

---
+++ controllers/base_controller.py
@@ -0,0 +1,9 @@
+from flask import Blueprint
+
+base_bp = Blueprint('base', __name__)
+
+class BaseController:
+    @staticmethod
+    @base_bp.route('/')
+    def index():
+        return 'Hello, this is the root route!'
