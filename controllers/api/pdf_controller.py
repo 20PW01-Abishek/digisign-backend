a09d4fed8b232a24fde7ca95b55fba375590acd9
karthikowo <20pw16@psgtech.ac.in>
2024-03-20 23:55:30 +0530

    add sharing endpoints

--- controllers/api/pdf_controller.py
+++ controllers/api/pdf_controller.py
@@ -73,7 +73,105 @@ class PDFController(BaseController):
             
             pdf_data = fs.get(ObjectId(file_id)).read()
             
-            # Send the PDF content as a response
             return Response(pdf_data, mimetype='application/pdf'), 200
+        except PyMongoError as e:
+            return jsonify({'error': str(e)}), 500
+
+    @staticmethod
+    @pdf_bp.route('/pdfs/share', methods=['POST'])
+    def share_pdf():
+        try:
+            emails = request.json.get('emails')
+            file_ids = request.json.get('file_ids')
+
+            if not emails or not file_ids:
+                return jsonify({'error': 'Emails and file IDs are required'}), 400
+
+            db_connection = DatabaseConnection(os.getenv("MONGODB_URI"))
+            client = db_connection.get_connection()
+            
+            db = client.digisign_activity
+            fs = GridFS(db)
+
+            shared_pdf_collection = db.fs.files
+
+            for file_id in file_ids:
+                pdf_metadata = shared_pdf_collection.find_one({'_id': ObjectId(file_id)})
+                if pdf_metadata:
+                    shared_to = pdf_metadata.get('shared_to', {})
+
+                    for email in emails:
+                        if email not in shared_to or shared_to[email] is True:
+                            shared_to[email] = False
+
+                    shared_pdf_collection.update_one(
+                        {'_id': ObjectId(file_id)},
+                        {'$set': {'shared_to': shared_to}}
+                    )
+
+            return jsonify({'message': 'PDFs shared successfully'}), 200
+        except PyMongoError as e:
+            return jsonify({'error': str(e)}), 500
+
+    @staticmethod
+    @pdf_bp.route('/pdfs/remove_share', methods=['POST'])
+    def remove_shared_access():
+        try:
+            emails = request.json.get('emails')
+            file_ids = request.json.get('file_ids')
+
+            if not emails or not file_ids:
+                return jsonify({'error': 'Emails and file IDs are required'}), 400
+
+            db_connection = DatabaseConnection(os.getenv("MONGODB_URI"))
+            client = db_connection.get_connection()
+            
+            db = client.digisign_activity
+            shared_pdf_collection = db.fs.files
+
+            for file_id in file_ids:
+                pdf_metadata = shared_pdf_collection.find_one({'_id': ObjectId(file_id)})
+                if pdf_metadata:
+                    
+                    for email in emails:
+                        if email in pdf_metadata.get('shared_to', {}):
+                            del pdf_metadata['shared_to'][email]
+                   
+                    shared_pdf_collection.update_one(
+                        {'_id': ObjectId(file_id)},
+                        {'$set': {'shared_to': pdf_metadata.get('shared_to', {})}}
+                    )
+
+            return jsonify({'message': 'Shared access removed successfully'}), 200
+        except PyMongoError as e:
+            return jsonify({'error': str(e)}), 500
+
+    @staticmethod
+    @pdf_bp.route('/pdfs/sent_for_signing', methods=['GET'])
+    def get_sent_for_signing_pdfs():
+        try:
+            user_id = request.args.get('user_id')
+
+            if not user_id:
+                return jsonify({'error': 'User ID is required as a query parameter'}), 400
+
+            db_connection = DatabaseConnection(os.getenv("MONGODB_URI"))
+            client = db_connection.get_connection()
+
+            db = client.digisign_activity
+            shared_pdf_collection = db.fs.files
+
+            sent_for_signing_pdfs = shared_pdf_collection.find({
+                'user_id': user_id,
+                'shared_to': {'$exists': True, '$ne': {}}
+            })
+
+            pdf_list = [{
+                'file_id': str(pdf['_id']),
+                'filename': pdf['filename'],
+                'shared_to': pdf.get('shared_to', {})
+            } for pdf in sent_for_signing_pdfs]
+
+            return jsonify({'pdfs': pdf_list}), 200
         except PyMongoError as e:
             return jsonify({'error': str(e)}), 500
