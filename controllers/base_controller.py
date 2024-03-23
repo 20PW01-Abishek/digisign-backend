cbdac3b81d8040ebcdb2ba0995ebb05fcfbfae8f
karthikowo <20pw16@psgtech.ac.in>
2024-03-23 10:04:02 +0530

    setup a before_request action

--- controllers/base_controller.py
+++ controllers/base_controller.py
@@ -1,9 +1,17 @@
 # controllers/base_controller.py
 from flask import Blueprint
-
+from middlewares.token_auth import check_token
 base_bp = Blueprint('base', __name__)
 
 class BaseController:
+    @staticmethod
+    @base_bp.before_request
+    def validate_token():
+        response = check_token()
+        if response:
+            return response
+
+    
     @staticmethod
     @base_bp.route('/')
     def index():
