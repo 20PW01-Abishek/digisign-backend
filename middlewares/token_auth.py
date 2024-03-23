ae352d1b39efdcab54815be62614beade5675048
karthikowo <20pw16@psgtech.ac.in>
2024-03-23 10:04:14 +0530

    setup token validation middleware

---
+++ middlewares/token_auth.py
@@ -0,0 +1,27 @@
+# middlewares/token_auth.py
+from flask import request, jsonify
+from firebase_admin import auth
+
+def verify_firebase_token(token):
+    try:
+        decoded_token = auth.verify_id_token(token)
+        return decoded_token
+    except Exception as e:
+        return None, str(e)
+
+def check_token():
+    token = request.headers.get('Authorization')
+    print(f"Auth token : {token}", not token)
+    if not token:
+        return jsonify({'message': 'Missing token'}), 401
+
+    verification_result = verify_firebase_token(token)
+
+    if isinstance(verification_result, tuple):
+        decoded_token, error_message = verification_result
+        if not decoded_token:
+            return jsonify({'message': error_message}), 401
+    else:
+        decoded_token = verification_result
+
+    return None
